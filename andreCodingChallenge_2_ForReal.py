a8d1a8ff36d953ec8fbdda667ce2cf3cae2b7253
EndlessConundrums <wilcoxgonzalezn27@bcdschool.org>
2025-04-10 21:02:12 +0000

    Started working on n-body physics sim

--- andreCodingChallenge_2_ForReal.py
+++ andreCodingChallenge_2_ForReal.py
@@ -1,35 +1,10 @@
-#password inputter time
-endSession = False
-username = input("Please input your username: ")
-password = input("Now please enter your password: ")
-while endSession is False:
-    action = input("What would you like to do? : ")
-    if action == "REVIEWPASS":
-        testPass = input("Okay! Input your password: ")
-        if testPass == password:
-            print("Review successful!")
-        else:
-            print("That was incorrect. Did you write your password down somewhere?")
-    elif action == "NEWPASS":
-        testPass = input("Input your current password first: ")
-        if testPass == "whte_rbt":
-            print("Hello, Dennis. Your account is unfortunately not present. We THINK you forgot to say the magic word.")
-        elif testPass == password:
-            newPass = input("Please enter your new password here: ")
-            password = newPass
-        else:
-            print("That was incorrect. Did you write your password down somewhere?")
-    elif action == "NEWUSER":
-        testPass = input("Input your current password first: ")
-        if testPass == "whte_rbt":
-            print("Hello, Dennis. Your account is unfortunately not present. We THINK you forgot to say the magic word.")
-        elif testPass == password:
-            newPass = input("Please enter your new username here: ")
-            username = newPass
-        else:
-            print("That was incorrect. Did you write your password down somewhere?")
-    elif action == "QUIT":
-        print("Okay. Program quitting.")
-        break
-    else:
-        print("Error: " + action + " was not recognized. Please try again.")
+import math
+massOne = [100,300,300,10,0] #first number is mass, next two are position, final two are velocity
+massTwo = [120,-300,-300,-10,0]
+numMasses = 2
+gravitationalConstant = 1 #DUMMY VALUE; REPLACE LATER
+while True:
+    xDist = massOne[1] - massTwo[1]
+    yDist = massOne[2] - massTwo[2]
+    distance = math.sqrt((xDist ** 2) + (yDist ** 2))
+    force = gravitationalConstant * ((massOne[0] * massTwo[0]) / (distance ** 2))
