ec7cd0198b05b562f40f93d97cbbc4249b3b42a3
EndlessConundrums <wilcoxgonzalezn27@bcdschool.org>
2025-04-28 21:03:40 +0000

    Added gravity function and point-mass class (plus started loop)

--- orbital_mechanics.py
+++ orbital_mechanics.py
@@ -1,10 +1,39 @@
 import math
-massOne = [100,300,300,10,0] #first number is mass, next two are position, final two are velocity
-massTwo = [120,-300,-300,-10,0]
-numMasses = 2
-gravitationalConstant = 1 #DUMMY VALUE; REPLACE LATER
+G = 1 # dummy value; must change later (but also don't use the actual value for ease of programming)
+def gravityEquation(mass1, mass2):
+    xDist = mass1.getX() - mass2.getX()
+    yDist = mass1.getY() - mass2.getY()
+    gforceX = G * ((mass1.getMass() * mass2.getMass())/(xDist ** 2)) # newton?
+    gforceY = G * ((mass1.getMass() * mass2.getMass())/(yDist ** 2)) # newton.
+    accel1X = (gforceX / mass1.getMass())
+    accel1Y = (gforceY / mass1.getMass())
+    accel2X = (gforceX / mass2.getMass())
+    accel2Y = (gforceY / mass2.getMass())
+    mass1.accelerate(accel1X, accel1Y)
+    mass2.accelerate(accel2X, accel2Y)
+
+class Body:
+    def __init__(self, mass, xPos, yPos):
+        self.mass = mass
+        self.x = xPos
+        self.y = yPos
+        self.velX = 0
+        self.velY = 0
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
+    def getMass(self):
+        return self.mass
+    def accelerate(self, accelX, accelY):
+        self.velX += accelX
+        self.velY += accelY
+    def update(self):
+        self.x += self.velX
+        self.y += self.velY
+
+Bogol = Body(175, 100, 100)
+Grumbill = Body(200, -250, -230)
+
 while True:
-    xDist = massOne[1] - massTwo[1]
-    yDist = massOne[2] - massTwo[2]
-    distance = math.sqrt((xDist ** 2) + (yDist ** 2))
-    force = gravitationalConstant * ((massOne[0] * massTwo[0]) / (distance ** 2))
+    print("Starting loop...")
